2f2208c1039ea1ae0c1fdcae0828af05e472d146
Brian Pham <phamhoanglinh1998@gmail.com>
2024-08-07 23:24:28 -0700

    Initial commit

---
+++ .history/.eslintrc_20240807181622.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  ignorePatterns: ['**/payload-types.ts'],
+  plugins: ['prettier'],
+  rule: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
+}
---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+
+import classes from './index.module.scss'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+  return (
+    <div>
+      <nav
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        <Gutter className={classes.wrap}>
+          <Link href={'/'}>
+            <Image
+              className={classes.logo}
+              src="/logo-black.svg"
+              alt="logo"
+              width={170}
+              height={'50'}
+            />
+          </Link>
+
+          <HeaderNav header={header}></HeaderNav>
+        </Gutter>
+      </nav>
+    </div>
+  )
+}
+
+export default HeaderComponent
